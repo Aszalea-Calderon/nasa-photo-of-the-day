879b7d7d879c3e1ae05f5724deef08df3d7f8737
Aszalea Calderon <aszalea_calderon@yahoo.com>
2020-11-04 16:00:39 -0800

    added everything

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Posts from "./components/Posts/posts";
+
 import "./App.css";
+// TASK 1 - import the axios lib from node_modules
+import axios from "axios";
+
+// TASK 2 - import the contants from constants/index.js
+import { BASE_URL, API_KEY } from "./constants/apiInfo.js";
 
 function App() {
+  //Setting the slices of State
+  const [posts, setPosts] = useState([]);
+
+  //Calling the Axios's
+  useEffect(() => {
+    axios
+      .get(`${BASE_URL}/planetary/apod?api_key=${API_KEY}`)
+      .then((res) => {
+        debugger;
+        // setFriends(res.data);
+      })
+      .catch((err) => {
+        debugger;
+
+        // console.log(err);
+      });
+  }, []);
+
+  //Doing stuff here, We are going to have a post that we can like
+  const likePost = (postId) => {
+    setPosts(
+      posts.map((item) => {
+        return item.id === postId ? { ...item, likes: item.likes + 1 } : item;
+      })
+    );
+  };
+
+  //Shipping it out
   return (
     <div className="App">
       <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+        <Posts likePost={likePost} posts={posts} />
+        Welcome to Nasa's Photo of the day!
+        <span role="img" aria-label="go!">
+          🚀
+        </span>
       </p>
     </div>
   );
---
+++ src/components/Posts/posts.js
@@ -0,0 +1,13 @@
+import React from "react";
+//import Post from "./posts.js"
+import "./posts.css";
+
+const Posts = (props) => {
+  //Set up the pass
+  const { likePost, posts } = props;
+  console.log(posts);
+
+  //Export it out
+  return <div></div>;
+};
+export default Posts;
